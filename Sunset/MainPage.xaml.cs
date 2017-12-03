54da4ef872c6fe9f84012b00109fc4645da0e244
liangminhua <ben302010@live.cn>
2017-12-03 23:11:13 +0800

    实现返回键，网页后退功能。

--- Sunset/MainPage.xaml.cs
+++ Sunset/MainPage.xaml.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +26,65 @@ namespace Sunset
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private SystemNavigationManager CurrentView;
         public MainPage()
         {
             this.InitializeComponent();
+            MainWebView.Source = new Uri("http://m.youku.com/");
+            MainWebView.NewWindowRequested += MainWebView_NewWindowRequested;
+            MainWebView.DOMContentLoaded += MainWebView_DOMContentLoaded;
+            MainWebView.UnsupportedUriSchemeIdentified += MainWebView_UnsupportedUriSchemeIdentified;
+            MainWebView.ContainsFullScreenElementChanged += MainWebView_ContainsFullScreenElementChanged;
+            CurrentView = SystemNavigationManager.GetForCurrentView();
+            CurrentView.BackRequested += CurrentView_BackRequested;
+        }
+
+        private void MainWebView_UnsupportedUriSchemeIdentified(WebView sender, WebViewUnsupportedUriSchemeIdentifiedEventArgs args)
+        {
+            args.Handled = true;
+        }
+
+        private void MainWebView_DOMContentLoaded(WebView sender, WebViewDOMContentLoadedEventArgs args)
+        {
+            string FixScript = @"window.login = function login(callback){window.location.href = 'https://account.youku.com?callback='+ encodeURIComponent(window.location.href);}; console.log(1);";
+            MainWebView.InvokeScriptAsync("eval", new string[] { FixScript });
+        }
+
+        private void MainWebView_ContainsFullScreenElementChanged(WebView sender, object args)
+        {
+            var applicationView = ApplicationView.GetForCurrentView();
+            if (sender.ContainsFullScreenElement)
+            {
+                applicationView.TryEnterFullScreenMode();
+            }
+            else if (applicationView.IsFullScreenMode)
+            {
+                applicationView.ExitFullScreenMode();
+            }
+        }
+
+
+        private void CurrentView_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            if (MainWebView.CanGoBack)
+            {
+                MainWebView.GoBack();
+                e.Handled = true;
+                return;
+            }
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            if (rootFrame.CanGoBack)
+            {
+                e.Handled = true;
+                rootFrame.GoBack();
+            }
+        }
+
+        private void MainWebView_NewWindowRequested(WebView sender, WebViewNewWindowRequestedEventArgs args)
+        {
+            MainWebView.Source = args.Uri;
+            args.Handled = true;
         }
     }
 }
